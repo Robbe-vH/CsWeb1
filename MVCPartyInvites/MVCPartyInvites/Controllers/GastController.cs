82adda0787b975a3bf4629d2e95fb5a2223aa802
Robbe vanHoppe <12200496@student.pxl.be>
2023-09-28 09:25:59 +0200

    Model validatie

---
+++ MVCPartyInvites/MVCPartyInvites/Controllers/GastController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPartyInvites.Models;
+
+namespace MVCPartyInvites.Controllers
+{
+    public class GastController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  IActionResult Reservatie(Gast gast)
+        {
+            if (ModelState.IsValid)
+            {
+                if (gast.Naam.Length < 2)
+                {
+                    ModelState.AddModelError("", "Naam is te kort");
+                    return View("Index", gast);
+                }
+                // LocalData.GastList.Add(gast);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View("Index", gast);
+            }
+        }
+    }
+}
