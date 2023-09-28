d6f4570489c270e7c9c74bcdfdcf98b94186006a
Robbe vanHoppe <12200496@student.pxl.be>
2023-09-28 13:06:28 +0200

    Added locations and clientlocation controllers + pages

---
+++ WebAppMvcClientLocation/WebAppMvcClientLocation/Controllers/LocationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMvcClientLocation.Data;
+using WebAppMvcClientLocation.Models;
+
+namespace WebAppMvcClientLocation.Controllers
+{
+    public class LocationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Database.Locations);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Location location)
+        {
+            location.LocationId = Database.Locations.Count;
+            if (ModelState.IsValid)
+            {
+
+                Database.AddLocation(location);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
